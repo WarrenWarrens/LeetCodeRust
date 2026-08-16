5de5890193336f37b0a2c53f3046939452e63d1f
Warren <warrener.ryan@gmail.com>
2026-08-16 09:49:09 -0400

    test2

--- src/arrays/two_sum.rs
+++ src/arrays/two_sum.rs
@@ -22,7 +22,7 @@ mod tests {
 
     #[test]
     fn test_two_sum() {
-        assert_eq!(Solution::two_sum(vec![1, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
         assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
     }
 }
